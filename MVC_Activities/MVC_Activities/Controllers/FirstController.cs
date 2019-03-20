d3af0e554d50ba5355760c18ebe5734adacdee1c
jamican squid <andrew.yooha.kim@gmail.com>
2019-03-20 11:24:03 -0500

    first step

---
+++ MVC_Activities/MVC_Activities/Controllers/FirstController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MVC_Activities.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+        public ActionResult Index()
+        {
+            return View(viewName: "Index", model: "All starts here.");
+        }
+
+        [HttpPost]
+        public ActionResult SayHello(string inputString)
+        {
+            return View(viewName: "Index", model: "Hello " + inputString);
+        }
+
+    }
+}
