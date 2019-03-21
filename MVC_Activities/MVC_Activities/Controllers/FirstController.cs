353045d18a6178905be6b1a0fca597e7b3e3f648
Tyler Tran <19trant@elmbrookstudents.org>
2019-03-21 09:55:30 -0500

    MVC activity 1

---
+++ MVC_Activities/MVC_Activities/Controllers/FirstController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DatabaseActivities.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+        public ActionResult Index()
+        {
+            return View(viewName: "Index", model: "All starts here.");
+        }
+
+        [HttpPost]
+        public ActionResult SayHello(string inputString)
+        {
+            return View(viewName: "Index", model: "Hello " + inputString);
+        }
+
+    }
+}
