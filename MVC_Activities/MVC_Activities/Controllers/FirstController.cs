6188b10f08273233598e30a1c32b1be1aa8a8c6f
jamican squid <andrew.yooha.kim@gmail.com>
2019-03-22 10:44:35 -0500

    Activity 2

--- MVC_Activities/MVC_Activities/Controllers/FirstController.cs
+++ MVC_Activities/MVC_Activities/Controllers/FirstController.cs
@@ -12,14 +12,27 @@ namespace MVC_Activities.Controllers
         // GET: First
         public ActionResult Index()
         {
-            return View(viewName: "Index", model: "All starts here.");
+            return View(viewName: "Index", model: "Cleaned Up Messege");
         }
 
         [HttpPost]
         public ActionResult SayHello(string inputString)
         {
-            return View(viewName: "Index", model: "Hello " + inputString);
+            return View(viewName: "Index", model: "Hello " + inputString + " your dumb");
         }
+
+        public ActionResult Square()
+        {
+            return View(viewName: "Square", model: 0.0);
+        }
+
+
+        [HttpPost]
+        public ActionResult SaySquare(double inputNumber)
+        {
+            return View(viewName: "Square", model: (Math.Sqrt(inputNumber)));
+        }
+
 
     }
 }
