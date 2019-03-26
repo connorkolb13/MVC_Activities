5bd680e9cafd1ed293fdba43645fda9dab4ed0e2
jamican squid <andrew.yooha.kim@gmail.com>
2019-03-26 10:16:11 -0500

    ok yeah

--- MVC_Activities/MVC_Activities/Controllers/FirstController.cs
+++ MVC_Activities/MVC_Activities/Controllers/FirstController.cs
@@ -33,6 +33,39 @@ namespace MVC_Activities.Controllers
             return View(viewName: "Square", model: (Math.Sqrt(inputNumber)));
         }
 
+        public ActionResult MagicNumber()
+        {
+            return View(viewName: "MagicNumber", model: 0);
+        }
+
+        [HttpPost]
+        public ActionResult FindNum(int inputNumberLow, int inputNumberHigh)
+        {
+            if (inputNumberHigh-inputNumberLow<=1)
+            {
+                return View(viewName: "MagicNumber", model: -999);
+            }
+            else
+            {
+                Random gen = new Random();
+                
+                return View(viewName: "MagicNumber", model: (gen.Next(inputNumberLow+1, inputNumberHigh)));
+            }
+        }
+        
+        public ActionResult Dates()
+        {
+            return View(viewName: "Dates", model: (DateTime.Now));
+        }
+
+
+        [HttpPost]
+        public ActionResult DateMagic(DateTime inputBirthday)
+        {
+            return View(viewName: "Dates", model: inputBirthday);
+        }
+
+
 
     }
 }
