9ad68abaf9d715f8d03cbcf42c76e53428e60289
jamican squid <andrew.yooha.kim@gmail.com>
2019-03-28 10:15:18 -0500

    java

---
+++ MVC_Activities/MVC_Activities/Controllers/ConversionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Activities.Controllers
+{
+    public class ConversionController : Controller
+    {
+        // GET: Conversion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
